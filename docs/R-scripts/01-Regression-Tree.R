7d894b608d06ce0b99933abf674d547ebb6ab942
DeivisonSouza <souzaflorestal@gmail.com>
2020-10-13 16:50:37 -0300

    Update R-script

--- docs/R-scripts/01-Regression-Tree.R
+++ docs/R-scripts/01-Regression-Tree.R
@@ -11,92 +11,84 @@ easypackages::libraries("caret","data.table","tidyverse",
                         "rpart", "rpart.plot")
 
 ## 3: Carrega conjunto de dados -------------------------------------------
-data <- fread("Tectona.csv", stringsAsFactors=T)
+data <- fread("./docs/R-scripts/Tectona.csv", stringsAsFactors=T)
 
 ## 4: Engenharia de recursos ----------------------------------------------
-data[,D2:=D^2
-     ][,lnD:=log(D)
-       ][,invD:=1/D
-         ][,D2H:=D^2*H
-           ][,lnH:=log(H)
-             ][,DH2:=D*H^2
-               ][,lnD2H:=log(D^2*H)
-                 ][,H2:=H^2
-                   ][,DH:=D*H]
+# data[,D2:=D^2
+#      ][,lnD:=log(D)
+#        ][,invD:=1/D
+#          ][,D2H:=D^2*H
+#            ][,lnH:=log(H)
+#              ][,DH2:=D*H^2
+#                ][,lnD2H:=log(D^2*H)
+#                  ][,H2:=H^2
+#                    ][,DH:=D*H]
 
 ## 5: Divisão aleatória estratificada ------------------------------------
 set.seed(100)
-trainIndex <- createDataPartition(y=data$Esp,p=.80,list=FALSE)
-trainingSet <- data[trainIndex,][,-"VR"]
-testSet <- data[-trainIndex,][,-"VR"]
+trainIndex <- createDataPartition(y=data$V, p=.70, list=FALSE)
+trainingSet <- data[trainIndex,]
+testSet <- data[-trainIndex,]
 
 ## 6: Configuração do treinamento ----------------------------------------
-source("Summary.R")
+source("./docs/R-scripts/Summary.R")
 
 fitControl <- trainControl(method="cv", number=10,
                            returnResamp="final", savePredictions=TRUE,
                            allowParallel=T, summaryFunction=Summary,
-                           verboseIter=T, selectionFunction="oneSE")
+                           verboseIter=T, selectionFunction="best")
 
 ## 7: Construção de modelos preditivos -----------------------------------
 
 ### 7.1: Hiperparâmetros candidatos --------------------------------------
-tuneGrid <- expand.grid(cp = seq(0.00001, 0.001, 0.00001))
+tuneGrid <- expand.grid(cp = seq(0.0001, 0.02, 0.001))
+
+tuneGrid <- expand.grid(kmax = seq(1,25,1),
+                      kernel = c("biweight","epanechnikov",
+                               "gaussian","triweight"),
+                      distance = 1:3)
 
 ### 7.2: Ajuste de hiperparâmetros (Validação cruzada)
 set.seed(1000)
-cartTuneD <- train(lnVR ~.,
-                   data = trainingSet[,c(2,4:7)],
-                   method = "rpart",
-                   tuneGrid = tuneGrid,
-                   trControl = fitControl)
+m_RT <- train(V ~., data = trainingSet,
+              method = "kknn",
+              tuneGrid = tuneGrid,
+              preProcess=c("center","scale", "BoxCox"),
+              trControl = fitControl)
 
-plot(cartTuneD)
-
-# Melhor configuaração?
-cartTuneD$bestTune
+# Melhor configuração (ou valor de cp)
+m_RT$bestTune
 
 # Plot
-plot(cartTuneD)
+plot(m_RT)
 
 # imprime as decisões da árvore final
-print(cartTuneD$finalModel)
-rpart.rules(cartTuneD$finalModel, style = "wide")
-rpart.rules(cartTuneD$finalModel, cover = TRUE)
-(py<- partykit::as.party(cartTuneD$finalModel))
+print(m_RT$finalModel)
+rpart.rules(m_RT$finalModel, style = "wide")
+rpart.rules(m_RT$finalModel, cover = TRUE)
+(py <- partykit::as.party(m_RT$finalModel))
 
 # Plota a árvore final
-plot(cartTuneD$finalModel)
-text(cartTuneD$finalModel)
+plot(m_RT$finalModel)
+text(m_RT$finalModel)
 
-rattle::fancyRpartPlot(cartTuneD$finalModel)
-rpart.plot::rpart.plot(cartTuneD$finalModel)
-
+rattle::fancyRpartPlot(m_RT$finalModel)
+rpart.plot::rpart.plot(m_RT$finalModel)
 plot(py)
 
-source("treePlot.R")
-treePlot(cartTuneD$finalModel, type = 2, extra = 101,
-         varlen = 0, faclen = 1, fallen.leaves = TRUE)
-
-# Medidas de desempenho médio?
-results.cartTuneD <- cartTuneD$results
-setorder(results.cartTuneD, RMSE)
-
-(x <- results.cartTuneD[results.cartTuneD$cp == cartTuneD$bestTune$cp, ])
+# Desempenho médio na validação cruzada
+results.m_RT <- m_RT$results
+setorder(results.m_RT, RMSE)
 
 # Variáveis importantes
-varImp(cartTuneD)
-ggplot(varImp(cartTuneD))
+varImp(m_RT)
+ggplot(varImp(m_RT))
 
 # Salva e ler os modelos
-saveRDS(cartTuneD,'m_RT.rds')
+saveRDS(m_RT,'m_RT.rds')
 # m_RT <- readRDS('m_RT.rds')
 
-# Desempenho no conjunto de treino
-data <- data.frame(obs = trainingSet$lnVR,
-                   pred = predict(cartTuneD, trainingSet))
-round(Summary(data=data), 4)
-
 # Desempenho no conjunto de teste
-dataT <- data.frame(obs = testSet$lnVR, pred = predict(cartTuneD, testSet))
-round(Summary(data=dataT), 4)
+(pred <- predict(m_RT, testSet))
+df <- data.frame(obs = testSet$V, pred = pred)
+round(Summary(data=df), 4)
